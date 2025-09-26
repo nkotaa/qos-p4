9e6f9ead1b6216668c7613cbe944fb2ea8b44dff
nkotaa <e0957774@u.nus.edu>
2025-09-26 11:06:10 +0800

    Minimal working server implementation

--- service/virtual_circuit_server_impl.py
+++ service/virtual_circuit_server_impl.py
@@ -3,19 +3,36 @@ import grpc
 from service import virtual_circuit_pb2
 from service import virtual_circuit_pb2_grpc
 
+def set_up_virtual_circuit(intserv, a_side_interface,
+                           b_side_interface, bandwidth_kbps):
+    try:
+        flow_selector = intserv.create_flow_selector(
+            ingress_port=a_side_interface.port,
+            source_vlanid=a_side_interface.vlan_id,
+            egress_port=b_side_interface.port,
+            dest_vlanid=b_side_interface.vlan_id,
+            )
+        intserv.set_bandwidth(flow_selector, bandwidth_kbps,
+                              service_class=2)
+    except RuntimeError as err:
+        raise RuntimeError("Virtual circuit not set up")
+
+def tear_down_virtual_circuit(intserv, a_side_interface):
+    raise NotImplementedError('Method not implemented!')
+
 class VirtualCircuitServicer(virtual_circuit_pb2_grpc.VirtualCircuitServicer):
 
-    def __init__(self, control_plane_interface):
-        self.control_plane_interface = control_plane_interface
+    def __init__(self, intserv, get_virtual_interface):
+        self.intserv = intserv
+        self.get_virtual_interface = get_virtual_interface
 
     def SetUp(self, request, context):
         is_virtual_circuit_set_up = False
         try:
-            self.control_plane_interface.set_up_virtual_circuit(
-                request.a_side_id,
-                request.b_side_id,
-                request.bandwidth_kbps,
-                )
+            a_side_interface = self.get_virtual_interface(member_id=request.a_side_id)
+            b_side_interface = self.get_virtual_interface(member_id=request.b_side_id)
+            set_up_virtual_circuit(self.intserv, a_side_interface,
+                                   b_side_interface, request.bandwidth_kbps)
             is_virtual_circuit_set_up = True
         except RuntimeError as err:
             raise NotImplementedError
@@ -26,9 +43,8 @@ class VirtualCircuitServicer(virtual_circuit_pb2_grpc.VirtualCircuitServicer):
     def TearDown(self, request, context):
         is_virtual_circuit_torn_down = False
         try:
-            self.control_plane_interface.tear_down_virtual_circuit(
-                request.a_side_id,
-                )
+            a_side_interface = self.get_virtual_interface(member_id=request.a_side_id)
+            tear_down_virtual_circuit(self.intserv, a_side_interface)
             is_virtual_circuit_torn_down = True
         except RuntimeError as err:
             raise NotImplementedError
