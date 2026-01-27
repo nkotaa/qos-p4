388c91d41c616729b86f9fe2f53327db2c95dc06
nkotaa <e0957774@u.nus.edu>
2026-01-27 13:08:21 +0800

    refactor: wrap switch program calls in switch_connection class
    Allows reuse by other controller modules.

--- controller/intserv.py
+++ controller/intserv.py
@@ -1,4 +1,3 @@
-import random
 from dataclasses import dataclass
 
 INGRESS_METER_TABLE_BFRUNTIME = "Ingress.flow_meters_ingress.execute_ig_meter"
@@ -15,84 +14,53 @@ class DataFlow:
 
 class BFRuntimeIntServ:
 
-    import bfrt_grpc.client as gc
-
-    def __init__(self, device_id=0, program_name=None, pipe_name='pipe'):
-        bfrt_interface = self.gc.ClientInterface(
-            grpc_addr='localhost:50052',
-            client_id=random.randint(9, 65535),
-            device_id=device_id,
-            perform_subscribe=False,
-            )
-        self.bfrt_info = bfrt_interface.bfrt_info_get(p4_name=program_name)
-        self.program_name = self.bfrt_info.p4_name_get()
-        self.dev_target = self.gc.Target(device_id)
-        self.ingress_meter_table = self.bfrt_info.table_get(
-            pipe_name + '.' + INGRESS_METER_TABLE_BFRUNTIME)
-        self.egress_meter_table = self.bfrt_info.table_get(
-            pipe_name + '.' + EGRESS_METER_TABLE_BFRUNTIME)
 
     def create_flow_selector(self, ingress_port=None, source_vlanid=None,
                              egress_port=None, dest_vlanid=None):
         flow = DataFlow(ingress_port, source_vlanid, egress_port, dest_vlanid)
         return flow
+    def __init__(self, switch_connection):
+        self.switch_connection = switch_connection
 
     def set_guaranteed(self, flow_selector, bandwidth_kbps):
-        ingress_meter_table_keys = [self.ingress_meter_table.make_key([
-            self.gc.KeyTuple('hdr.vlan.vid', flow_selector.source_vlanid),
-            self.gc.KeyTuple('meta.pipe_id', pipe_id),
-            self.gc.KeyTuple('ig_tm_md.ucast_egress_port',
-                        flow_selector.egress_port),
-            ]
-            ) for pipe_id in range(2)]
-        ingress_meter_table_data = self.ingress_meter_table.make_data([
-            self.gc.DataTuple('$METER_SPEC_CIR_KBPS', 0),
-            self.gc.DataTuple('$METER_SPEC_PIR_KBPS', 25000000),
-            self.gc.DataTuple('$METER_SPEC_CBS_KBITS', 100000),
-            self.gc.DataTuple('$METER_SPEC_PBS_KBITS', 1000000),
-            ],
-            'Ingress.flow_meters_ingress.invoke_flow_meter'
-            )
-        egress_meter_table_key = self.egress_meter_table.make_key([
-            self.gc.KeyTuple('hdr.vlan.vid', flow_selector.source_vlanid),
-            self.gc.KeyTuple('eg_intr_md.egress_port',
-                             flow_selector.egress_port),
-            ]
-            )
-        egress_meter_table_data = self.egress_meter_table.make_data([
-            self.gc.DataTuple('$METER_SPEC_CIR_KBPS', bandwidth_kbps),
-            self.gc.DataTuple('$METER_SPEC_PIR_KBPS', 25000000),
-            self.gc.DataTuple('$METER_SPEC_CBS_KBITS', 100000),
-            self.gc.DataTuple('$METER_SPEC_PBS_KBITS', 1000000),
-            ],
-            'Egress.flow_meters_egress.invoke_flow_meter'
-            )
-
-        self.ingress_meter_table.entry_add(
-            self.dev_target, ingress_meter_table_keys,
-            [ingress_meter_table_data, ingress_meter_table_data],
-            p4_name = self.program_name)
-        self.egress_meter_table.entry_add(
-            self.dev_target, [egress_meter_table_key],
-            [egress_meter_table_data], p4_name = self.program_name)
+        ingress_match_list = [{
+            "hdr.vlan.vid": flow_selector.source_vlanid,
+            "meta.pipe_id": pipe_id,
+            "ig_tm_md.ucast_egress_port": flow_selector.egress_port,
+            } for pipe_id in range(2)]
+        ingress_action = ('Ingress.flow_meters_ingress.invoke_flow_meter', {
+            "$METER_SPEC_CIR_KBPS": 0,
+            "$METER_SPEC_PIR_KBPS": 25000000,
+            "$METER_SPEC_CBS_KBITS": 100000,
+            "$METER_SPEC_PBS_KBITS": 1000000,
+            })
+        egress_match = {
+            "hdr.vlan.vid": flow_selector.source_vlanid,
+            "eg_intr_md.egress_port": flow_selector.egress_port,
+            }
+        egress_action = ('Egress.flow_meters_egress.invoke_flow_meter', {
+            "$METER_SPEC_CIR_KBPS": bandwidth_kbps,
+            "$METER_SPEC_PIR_KBPS": 25000000,
+            "$METER_SPEC_CBS_KBITS": 100000,
+            "$METER_SPEC_PBS_KBITS": 1000000,
+            })
+        self.switch_connection.insert_table_entry(
+            INGRESS_METER_TABLE_BFRUNTIME, ingress_match_list,
+            [ingress_action, ingress_action])
+        self.switch_connection.insert_table_entry(
+            EGRESS_METER_TABLE_BFRUNTIME, [egress_match], [egress_action])
 
     def set_best_effort(self, flow_selector):
-        ingress_meter_table_keys = [self.ingress_meter_table.make_key([
-            self.gc.KeyTuple('hdr.vlan.vid', flow_selector.source_vlanid),
-            self.gc.KeyTuple('meta.pipe_id', pipe_id),
-            self.gc.KeyTuple('ig_tm_md.ucast_egress_port',
-                        flow_selector.egress_port),
-            ]
-            ) for pipe_id in range(2)]
-        egress_meter_table_key = self.egress_meter_table.make_key([
-            self.gc.KeyTuple('hdr.vlan.vid', flow_selector.source_vlanid),
-            self.gc.KeyTuple('eg_intr_md.egress_port',
-                             flow_selector.egress_port),
-            ]
-            )
-        self.ingress_meter_table.entry_del(
-            self.dev_target, ingress_meter_table_keys,
-            p4_name = self.program_name)
-        self.egress_meter_table.entry_del(
-            self.dev_target, [egress_meter_table_key],
-            p4_name = self.program_name)
+        ingress_match_list = [{
+            "hdr.vlan.vid": flow_selector.source_vlanid,
+            "meta.pipe_id": pipe_id,
+            "ig_tm_md.ucast_egress_port": flow_selector.egress_port,
+            } for pipe_id in range(2)]
+        egress_match = {
+            "hdr.vlan.vid": flow_selector.source_vlanid,
+            "eg_intr_md.egress_port": flow_selector.egress_port,
+            }
+        self.switch_connection.remove_table_entry(
+            INGRESS_METER_TABLE_BFRUNTIME, ingress_match_list)
+        self.switch_connection.remove_table_entry(
+            EGRESS_METER_TABLE_BFRUNTIME, [egress_match])
---
+++ controller/switch_connection.py
@@ -0,0 +1,52 @@
+import random
+
+try:
+    import bfrt_grpc.client as gc
+except ImportError:
+    import os
+    import sys
+    PYTHON3_VER = '{}.{}'.format(sys.version_info.major, sys.version_info.minor)
+    SDE_PYTHON3 = os.path.join(os.getenv('SDE_INSTALL'), 'lib',
+                               'python'+PYTHON3_VER, 'site-packages')
+    sys.path.append(SDE_PYTHON3)
+    sys.path.append(os.path.join(SDE_PYTHON3, 'tofino'))
+    sys.path.append(os.path.join(SDE_PYTHON3, 'tofino', 'bfrt_grpc'))
+    import bfrt_grpc.client as gc
+
+class BFRuntimeSwitchConnection:
+
+    def __init__(self, grpc_addr='localhost:50052', device_id=0,
+                 program_name=None, pipe_name='pipe'):
+        bfrt_interface = gc.ClientInterface(
+            grpc_addr=grpc_addr,
+            client_id=random.randint(9, 65535),
+            device_id=device_id,
+            perform_subscribe=False,
+            )
+        self.dev_target = gc.Target(device_id)
+        self.bfrt_info = bfrt_interface.bfrt_info_get(p4_name=program_name)
+        self.program_name = self.bfrt_info.p4_name_get()
+        self.pipe_name = pipe_name
+
+    def _table_get(self, table_name):
+        return self.bfrt_info.table_get(self.pipe_name + '.' + table_name)
+
+    def insert_table_entry(self, table_name, match_list, action_list):
+        table_object = self._table_get(table_name)
+        table_key_list = [table_object.make_key(
+            [gc.KeyTuple(key, value) for key, value in match.items()]
+            ) for match in match_list]
+        table_action_list = [table_object.make_data(
+            [gc.DataTuple(key, value) for key, value in action_data.items()],
+            action_code
+            ) for action_code, action_data in action_list]
+        table_object.entry_add(self.dev_target, table_key_list,
+                               table_action_list, p4_name=self.program_name)
+
+    def remove_table_entry(self, table_name, match_list):
+        table_object = self._table_get(table_name)
+        table_key_list = [table_object.make_key(
+            [gc.KeyTuple(key, value) for key, value in match.items()]
+            ) for match in match_list]
+        table_object.entry_del(self.dev_target, table_key_list,
+                               p4_name=self.program_name)
