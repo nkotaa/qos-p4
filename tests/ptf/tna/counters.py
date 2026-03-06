32b81c159c24061b4eaa96de5d8f7b69bc6b9304
nkotaa <e0957774@u.nus.edu>
2026-03-06 11:35:25 +0800

    test: update monitor controller test

--- tests/ptf/tna/counters.py
+++ tests/ptf/tna/counters.py
@@ -15,7 +15,6 @@ except ImportError:
     sys.path.append(os.path.join(TESTDIR, '..', '..', '..'))
 from controller.switch_connection import BFRuntimeSwitchConnection
 from controller.monitor import BFRuntimeMonitor
-from controller.flow import DataFlow
 
 program_name = testutils.test_param_get("p4_name")
 pkt_len = int(testutils.test_param_get("pkt_len", "128"))
@@ -34,26 +33,28 @@ class MonitorControllerTest(FlowTest):
     def setUp(self):
         FlowTest.setUp(self)
         self.monitor = _set_up_monitor_controller()
-        self.flow_selector = DataFlow(ingress_port=ingress_port,
-                                      egress_port=egress_port)
 
     def runTest(self):
         pkt = testutils.simple_tcp_packet(eth_src=source_mac, eth_dst=dest_mac,
                                           pktlen=pkt_len)
         super().check_port_forwarding(pkt)
 
-        self.monitor.start_counter(self.flow_selector)
-        testutils.send_packet(self, ingress_port, pkt, count=num_pkts)
-        testutils.verify_each_packet_on_each_port(self, [pkt, pkt],
-                                                  [egress_port, egress_port])
-        rx_count = self.monitor.read_rx_counter(self.flow_selector)
+        self.monitor.start_rx_counter(self.flow_id)
+        self.monitor.start_tx_counter(self.flow_id)
+        testutils.send_packet(self, self.ingress_port, pkt, count=num_pkts)
+        testutils.verify_each_packet_on_each_port(self, [pkt] * num_pkts,
+                                                  [self.egress_port] * num_pkts)
+        rx_count = self.monitor.read_rx_counter(self.flow_id)
+        tx_count = self.monitor.read_tx_counter(self.flow_id)
 
         # Default packet size is pkt_len bytes and model adds 4 bytes of CRC
         pkt_size = pkt_len + 4
         expected_byte_count = num_pkts * pkt_size
-        logger.info("bytes sent = %s received count = %s",
-                    str(expected_byte_count), str(rx_count))
+        logger.info(
+            "bytes sent = %s, received count = %s, transmitted count = %s",
+            str(expected_byte_count), str(rx_count), str(tx_count))
 
     def tearDown(self):
-        self.monitor.stop_counter(self.flow_selector)
+        self.monitor.stop_rx_counter(self.flow_id)
+        self.monitor.stop_tx_counter(self.flow_id)
         FlowTest.tearDown(self)
