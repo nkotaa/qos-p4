6083d7b3d1f50a668db230904b431cedf9ddfa76
nkotaa <e0957774@u.nus.edu>
2026-04-22 08:42:20 +0800

    test: update int program test
    Subclass as more general sample program test.

--- tests/ptf/tna/int.py
+++ tests/ptf/tna/int.py
@@ -8,35 +8,71 @@ from base_test import FlowTest
 
 program_name = testutils.test_param_get("p4_name")
 num_pkts = int(testutils.test_param_get("num_pkts", "1001"))
-source_mac = testutils.test_param_get("src_mac", "11:33:55:77:99:00")
-dest_mac = testutils.test_param_get("dst_mac", "00:11:22:33:44:55")
 mirror_session_id = int(testutils.test_param_get("mirr_sesid", 13))
+mirror_type = int(testutils.test_param_get("mirr_type", 3))
+sflow_freq = int(testutils.test_param_get("sflow_freq", "512"))
 
 logger = misc_utils.get_logger()
 
-class INTProgramTest(FlowTest):
+EGR_PORT_MIRROR = 1
 
-    def _program_int_source_table(self):
-        logger.info("Programming int source table for the test...")
-        data2 = self.int_source.make_data(
-            [], "Ingress.int_source_ingress.stage_metadata")
-        self.int_source.entry_add(self.dev_target, [self.key2], [data2])
+class SampleProgramTest(FlowTest):
 
     def _program_sample_flow_table(self):
         logger.info("Programming sample flow table for the test...")
-        data3 = self.sample_flow_count.make_data(
-            [], "Egress.int_event_egress.trigger_sflow_count")
-        self.sample_flow_count.entry_add(self.dev_target, [self.key3], [data3])
+        self.sample_flow_count.info.key_field_annotation_add(
+            'hdr.ethernet.src_addr', 'mac')
+        key = self.sample_flow_count.make_key([
+            gc.KeyTuple('hdr.ethernet.src_addr', self.source_mac)])
+        data = self.sample_flow_count.make_data(
+            # separate flow_id from watchlist, use same value for convenience
+            [gc.DataTuple('flow_id', self.flow_id)],
+            "Egress.ixp_egr_tail.packet_sample_egress.trigger_sflow_count")
+        self.sample_flow_count.entry_add(self.dev_target, [key], [data])
 
-    def _program_int_event_table(self):
-        logger.info("Programming int event table for the test...")
-        data4 = self.int_event_trigger.make_data(
+    def _program_sample_event_table(self):
+        logger.info("Programming sample event table for the test...")
+        key = self.sample_flow_event.make_key([
+            gc.KeyTuple('flow_idx', self.flow_id),
+            gc.KeyTuple('ixp_eg_res.sflow_count', value=0, mask=sflow_freq-1),
+            gc.KeyTuple('eg_dprsr_md.drop_ctl', value=0, mask=0)])
+        data = self.sample_flow_event.make_data(
+            [], "Egress.ixp_egr_tail.packet_sample_egress.mirror_sample")
+        self.sample_flow_event.entry_add(self.dev_target, [key], [data])
+
+    def setUp(self):
+        FlowTest.setUp(self)
+        self.dev_target = gc.Target(device_id=0)
+        self.bfrt_info = self.interface.bfrt_info_get(program_name)
+        self.sample_flow_count = self.bfrt_info.table_get(
+            "Egress.ixp_egr_tail.packet_sample_egress.sample_flow_count")
+        self.sample_flow_event = self.bfrt_info.table_get(
+            "Egress.ixp_egr_tail.packet_sample_egress.sample_flow_event")
+
+    # Remove all table entries
+    def _clean_up(self):
+        self.sample_flow_count.entry_del(self.dev_target, [])
+        self.sample_flow_event.entry_del(self.dev_target, [])
+
+    def tearDown(self):
+        self._clean_up()
+        FlowTest.tearDown(self)
+
+class INTEventTest(SampleProgramTest):
+
+    def _program_sample_mirror_table(self):
+        logger.info("Programming sample mirror table for the test...")
+        key = self.sample_mirror.make_key([
+            gc.KeyTuple('mirror_type', EGR_PORT_MIRROR)])
+        data = self.sample_mirror.make_data(
             [gc.DataTuple('int_mirror_session', mirror_session_id)],
             "Egress.int_event_egress.mirror")
-        self.int_event_trigger.entry_add(self.dev_target, [self.key4], [data4])
+        self.sample_mirror.entry_add(self.dev_target, [key], [data])
 
     def _program_mirror_session_table(self):
         logger.info("Programming mirror session table for the test...")
+        key = self.mirror_cfg.make_key([
+            gc.KeyTuple('$sid', mirror_session_id)])
         mirror_cfg_bfrt_data = self.mirror_cfg.make_data([
             gc.DataTuple('$direction', str_val="EGRESS"),
             gc.DataTuple('$ucast_egress_port', 68),
@@ -44,54 +80,28 @@ class INTProgramTest(FlowTest):
             gc.DataTuple('$session_enable', bool_val=True),
             gc.DataTuple('$max_pkt_len', 1000),
             ], "$normal")
-        self.mirror_cfg.entry_add(self.dev_target, [self.key5], [ mirror_cfg_bfrt_data ])
-
-    def setUp(self):
-        FlowTest.setUp(self)
-        self.dev_target = gc.Target(device_id=0)
-        self.bfrt_info = self.interface.bfrt_info_get(program_name)
-        self.int_source = self.bfrt_info.table_get(
-            "Ingress.int_source_ingress.int_source")
-        self.sample_flow_count = self.bfrt_info.table_get(
-            "Egress.int_event_egress.sample_flow_count")
-        self.int_event_trigger = self.bfrt_info.table_get(
-            "Egress.int_event_egress.int_event_trigger")
-        self.mirror_cfg = self.bfrt_info.table_get("$mirror.cfg")
-        self.key2 = self.int_source.make_key([
-            gc.KeyTuple('flow_id', self.flow_id)])
-        self.key3 = self.sample_flow_count.make_key([
-            gc.KeyTuple('flow_id', self.flow_id)])
-        self.key4 = self.int_event_trigger.make_key([
-            gc.KeyTuple('is_sflow', True)])
-        self.key5 = self.mirror_cfg.make_key([
-            gc.KeyTuple('$sid', mirror_session_id)])
-
-    # Remove all table entries
-    def _clean_up(self):
-        self.int_source.entry_del(self.dev_target, [])
-        self.sample_flow_count.entry_del(self.dev_target, [])
-        self.int_event_trigger.entry_del(self.dev_target, [])
-        self.mirror_cfg.entry_del(self.dev_target, [])
-
-    def tearDown(self):
-        self._clean_up()
-        FlowTest.tearDown(self)
-
-class INTEventTest(INTProgramTest):
+        self.mirror_cfg.entry_add(self.dev_target, [key], [mirror_cfg_bfrt_data])
 
     def setUp(self):
         super().setUp()
+        self.sample_mirror = self.bfrt_info.table_get(
+            "Egress.int_event_egress.sample_mirror")
+        self.mirror_cfg = self.bfrt_info.table_get("$mirror.cfg")
 
     def runTest(self):
-        pkt = testutils.simple_tcp_packet(eth_src=source_mac, eth_dst=dest_mac,
-                                          dl_vlan_enable=True, vlan_vid=self.vlan_vid)
-        super().check_port_forwarding(pkt)
+        pkt = testutils.simple_tcp_packet(
+            eth_src=self.source_mac, eth_dst=self.dest_mac,
+            dl_vlan_enable=True, vlan_vid=self.vlan_vid)
+        FlowTest.program_mac_forward_table(self)
+        FlowTest.check_port_forwarding(self, pkt)
 
-        self._program_int_source_table()
+        FlowTest.program_flow_watchlist_table(self)
         self._program_sample_flow_table()
-        self._program_int_event_table()
+        self._program_sample_event_table()
+        self._program_sample_mirror_table()
         self._program_mirror_session_table()
 
+        logger.info("Sending packets...")
         testutils.send_packet(self, self.ingress_port, pkt, count=num_pkts)
         testutils.verify_each_packet_on_each_port(self, [pkt] * num_pkts,
                                                   [self.egress_port] * num_pkts)
@@ -106,4 +116,6 @@ class INTEventTest(INTProgramTest):
                 print(data_dict)
 
     def tearDown(self):
+        self.sample_mirror.entry_del(self.dev_target, [])
+        self.mirror_cfg.entry_del(self.dev_target, [])
         super().tearDown()
